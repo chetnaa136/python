549d00fa0d06b142c0da389224e3c8d2bcd5747e
chetnaa136 <deshmukhchetna37@gmail.com>
2025-04-15 18:21:05 +0530

    ok1

--- transferstatement.py
+++ transferstatement.py
@@ -1,13 +1,27 @@
-x=int(input("enter number 1:"))
-y=int(input("enter number 2:"))
-z=int(input("enter number 3:"))
-p=int(input("enter number 4:"))
-h=max(x,y,z,p)
-print(h)
-while True:
-    if h%x==0 and h%y==0 and h%z==0 and h%p==0:
-        lcm=h
-        break
-    h=h+1
-print(f'lcm of given values {x},{y},{z} and {p} is {h} ')
-    
+# x=int(input("enter number 1:"))
+# y=int(input("enter number 2:"))
+# z=int(input("enter number 3:"))
+# p=int(input("enter number 4:"))
+# h=max(x,y,z,p)
+# print(h)
+# while True:
+#     if h%x==0 and h%y==0 and h%z==0 and h%p==0:
+#         lcm=h
+#         break
+#     h=h+1
+# print(f'lcm of given values {x},{y},{z} and {p} is {h} ')
+# break pass and continue
+# x=int(input("enter number 1:"))
+# y=int(input("enter number 2:"))
+# if x>y:
+#     pass
+# print("hello")
+n=10
+i=0
+while i<=10:
+    if i==5:
+        i=i+1
+        continue
+    print(i)
+    i=i+1
+
